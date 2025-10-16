17c232e0bef76070d962a214a7003e2f2bdcb196
Sriram <kuramdasubunny@gmail.com>
2025-10-16 15:40:40 +0530

    search

--- index.js
+++ index.js
@@ -27,6 +27,8 @@
 function searchProducts() {
     const searchValue = document.getElementById("searchInput").value.toLowerCase()
     const categories = document.querySelectorAll(".category")
+    const topProductsEl = document.getElementById("topProducts")
+    let anyMatch = false;
 
     categories.forEach(category => {
         const categoryTitle = category.querySelector(".category-title").textContent.toLowerCase()
@@ -35,17 +37,25 @@ function searchProducts() {
 
         products.forEach(product => {
             const productName = product.querySelector(".product-name").textContent.toLowerCase()
-
             if(productName.includes(searchValue) || categoryTitle.includes(searchValue)) {
                 product.style.display = "block"
+                anyMatch = true
                 hasVisibleProducts = true
             }
             else {
                 product.style.display = "none"
+                
             }
         })
 
+
         category.style.display = hasVisibleProducts || searchValue === '' ? 'block' : 'none';
     })
+    if(searchValue === "" || !anyMatch) {
+        topProductsEl.style.display = "flex"
+    }
+    else {
+        topProductsEl.style.display = "none"
+    }
+    }
 
-}
